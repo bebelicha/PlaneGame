f057a14d571575ac2b7cde875071324b0ea84872
Isabela <isablafgm@gmail.com>
2025-04-03 13:49:52 -0300

    date object

--- Assets/Scripts/Webgl/DateReceiver.cs
+++ Assets/Scripts/Webgl/DateReceiver.cs
@@ -3,13 +3,27 @@ using UnityEngine.UI;
 
 public class DateReceiver : MonoBehaviour
 {
-    public Text textField; 
+    public Text textField;
 
-    public void Date(string formattedDate)
+    [System.Serializable]
+    public class DateObject
     {
+        public string type;       
+        public float value;       
+        public int resolution;   
+        public long captureTime;  
+    }
+
+    public void Date(string jsonData)
+    {
+        DateObject dateObject = JsonUtility.FromJson<DateObject>(jsonData);
+
         if (textField != null)
         {
-            textField.text = formattedDate;
+            textField.text = $"Type: {dateObject.type}\n" +
+                             $"Value: {dateObject.value}\n" +
+                             $"Resolution: {dateObject.resolution}\n" +
+                             $"CaptureTime: {dateObject.captureTime}";
         }
         else
         {
